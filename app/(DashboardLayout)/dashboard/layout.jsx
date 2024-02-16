5a4680130c34d0b1e226415e6279066706aeb716
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-02-16 14:41:22 +0300

    store page and components updated.

--- app/(DashboardLayout)/dashboard/layout.jsx
+++ app/(DashboardLayout)/dashboard/layout.jsx
@@ -1,30 +1,22 @@
-'use client'
-import Navbar from '@/components/dashboard/Navbar'
-import Sidebar from '@/components/dashboard/Sidebar'
-import React, { useState } from 'react'
+'use client';
+import Navbar from '@/components/dashboard/Navbar';
+import Sidebar from '@/components/dashboard/Sidebar';
+import React, { useState } from 'react';
 
 const MainLayout = ({ children }) => {
-    const [sideOpen, setSideOpen] = useState(false)
+  return (
+    <div className='flex h-screen w-full overflow-hidden bg-background pb-4'>
+      <Sidebar />
 
-    const changeSideBar = () => {
-        setSideOpen(prev => !prev)
-    }
+      <div className='flex flex-1 w-full flex-col h-full px-4 overflow-hidden gap-2'>
+        <Navbar />
 
-    return (
-        <div className="flex h-screen w-full overflow-hidden bg-background pb-4">
-            <Sidebar/>
-
-            <div className="flex flex-1 w-full flex-col h-full px-4 overflow-hidden gap-2">
-                <Navbar/>
-
-                <div className="flex flex-1 h-full overflow-auto px-2 pt-2 rounded-lg">
-                    <div className="py-6 w-full pl-6 md:pl-0">
-                    {children}
-                    </div>
-                </div>
-            </div>
+        <div className='flex flex-1 h-full overflow-auto px-2 pt-2 rounded-lg'>
+          <div className='py-6 w-full pl-6 md:pl-0'>{children}</div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- app/(DashboardLayout)/dashboard/store/page.jsx
+++ app/(DashboardLayout)/dashboard/store/page.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import {StoreColumn} from "@/lib/table/table";
-import {addStore, langs} from "@/lib/table/data";
-import AddPerson from '../Addperson/page';
+import React from 'react';
+import { StoreColumn } from '@/lib/table/table';
+import { langs } from '@/lib/table/data';
 import CustomTable from '@/components/table/CustomTable';
 
 const Store = () => {
   return (
+    <>
+      <CustomTable
+        api_route='/store'
+        columns={StoreColumn}
+        langs={langs}
+        perPage={10}
+        pagination={true}
+        paginationType='page'
+        defaultLang='Us'
+      />
+    </>
+  );
+};
 
-      <div>
-    <CustomTable   columns={StoreColumn}
-                    langs={langs}
-                    initial_dt={addStore}
-                    perPage={10}
-                    pagination={true}
-                    paginationType="page"
-                    defaultLang="Us"/>
-    </div>
-  )
-}
-
-export default Store
+export default Store;
