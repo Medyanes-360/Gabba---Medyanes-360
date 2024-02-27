b89acd4456174983c7975d77fb13137902c99d10
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-02-27 13:08:58 +0300

    stock components and container added.

--- app/(DashboardLayout)/dashboard/Stock/page.jsx
+++ app/(DashboardLayout)/dashboard/Stock/page.jsx
@@ -1,22 +1,7 @@
-import CustomTable from '@/components/table/CustomTable'
-import { addStock, langs } from '@/lib/table/data'
-import { StockColumn } from '@/lib/table/table'
-import React from 'react'
+import StockContainer from '@/containers/stock';
 
-const Stock = () => {
-  return (
-    <div>
-        <CustomTable
-         columns={StockColumn}
-         langs={langs}
-         initial_dt={addStock}
-         perPage={10}
-         pagination={true}
-         paginationType="page"
-         defaultLang="Us"/>
+const StockPage = () => {
+  return <StockContainer />;
+};
 
-    </div>
-  )
-}
-
-export default Stock
+export default StockPage;
---
+++ components/stock/index.jsx
@@ -0,0 +1,129 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import LoadingScreen from '@/components/other/loading';
+import ListProducts from './listProducts';
+import { getAPI } from '@/services/fetchAPI';
+import FinancialManagementCalculate from '@/functions/others/financialManagementCalculate';
+import { BiFilterAlt } from 'react-icons/bi';
+import { FaFileInvoice } from 'react-icons/fa';
+
+const Stock = () => {
+  const [showOrderOffer, setShowOrderOffer] = useState(false);
+  const [hiddenBasketBar, setHiddenBasketBar] = useState(false);
+  const [isloading, setIsloading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [productFeatures, setProductFeatures] = useState([]);
+  const [showBasketOffer, setShowBasketOffer] = useState(false);
+  const [isCustomerAndPersonel, setIsCustomerAndPersonel] = useState(false);
+  const [allFeatureValues, setAllFeatureValues] = useState([]);
+
+  // Yazdırma ekranına gönderilecek prop
+  const [selectedOrder, setSelectedOrder] = useState([]);
+
+  // Sepetteki ürünleri tuttuğumuz state.
+  const [basketData, setBasketData] = useState([]);
+  // Sepetteki ürünlerin stok değerlerini tuttuğumuz state.
+  const getData = async () => {
+    try {
+      setIsloading(true);
+      const response = await getAPI('/createProduct/createProduct');
+      if (!response) {
+        throw new Error('Veri çekilemedi 2');
+      }
+
+      if (response.status !== 'success') {
+        throw new Error('Veri çekilemedi');
+      }
+
+      // response.data.createProducts içerisindeki değerleri gez ve "productName" değerlerine göre küçükten büyüğe doğru sırala.
+      await response.data.createProducts.sort((a, b) =>
+        a.productName.localeCompare(b.productName)
+      );
+      await Promise.all(
+        await response.data.createProducts.map(async (item) => {
+          const { result } = await FinancialManagementCalculate(
+            item.productPrice
+          );
+          item.productPrice = result[result.length - 1];
+        })
+      );
+      setProducts(response.data.createProducts);
+      setProductFeatures(response.data.productFeatures);
+      setIsloading(false);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  async function getAllBasketData() {
+    const response = getAPI('/createOffer/basket');
+    const [dataResult] = await Promise.all([response]);
+    setIsloading(false);
+    setBasketData(dataResult.data);
+  }
+
+  useEffect(() => {
+    getData();
+    getAllBasketData();
+  }, []);
+
+  console.log('selectedOrder', selectedOrder);
+
+  return (
+    <>
+      {isloading && <LoadingScreen isloading={isloading} />}
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='dark'
+      />
+
+      <div
+        className={`${
+          hiddenBasketBar ? 'hidden' : 'flex'
+        } flex-col md:flex-row p-2 lg:p-2 lg:px-10 w-full justify-between mb-4 items-center shadow-lg  bg-gray-100 gap-2 pr-4`}
+      >
+        {/* Filtreleme ve Teklifler Butonu */}
+        <div className='flex justify-center item-center flex-col md:flex-row gap-2 px-4 my-2'>
+          <button className='bg-green-500 p-4 text-white rounded lg:text-lg flex flex-row gap-2 flex-nowrap hover:cursor-pointer hover:scale-105 transition-all mt-2 lg:mt-0'>
+            <BiFilterAlt size={25} />
+            Filtrele
+          </button>
+          <button
+            onClick={() => {
+              setShowOrderOffer(true);
+              setShowBasketOffer(false);
+            }}
+            className='bg-purple-600 p-4 text-white rounded lg:text-lg flex flex-row gap-2 flex-nowrap hover:cursor-pointer hover:scale-105 transition-all mt-2 lg:mt-0'
+          >
+            <FaFileInvoice size={25} />
+            Stoklar
+          </button>
+        </div>
+      </div>
+
+      <ListProducts
+        getData={getData}
+        getAllBasketData={getAllBasketData}
+        toast={toast}
+        isloading={isloading}
+        setIsloading={setIsloading}
+        products={products}
+        productFeatures={productFeatures}
+        setHiddenBasketBar={setHiddenBasketBar}
+        setAllFeatureValues={setAllFeatureValues}
+        allFeatureValues={allFeatureValues}
+      />
+    </>
+  );
+};
+
+export default Stock;
