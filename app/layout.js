10558e346878d909e45869017ecd2d1ca896464d
yusuf <ysfyldz0077@gmail.com>
2024-02-27 13:41:36 +0300

    fix ui

--- app/layout.js
+++ app/layout.js
@@ -51,7 +51,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang='en'>
-      <body className={classNames(inter.className)}>
+      <body className={classNames(inter.className, "bg-white")}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
