82f5f093e126877b08d0489918430544a29c6db1
yusuf <ysfyldz0077@gmail.com>
2024-02-28 14:48:47 +0300

    .

--- app/stepbystep/page.jsx
+++ app/stepbystep/page.jsx
@@ -3,11 +3,22 @@ import React, { useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 const StepByStep = () => {
+  const data = {
+    "user": {
+      username: "Yusuf"
+    },
+    "order": {
+      orderId: "00000"
+    }
+  }
   const [currentStep, setCurrentStep] = useState(0)
   const [steps, setSteps] = useState([
     {
       id: 0,
       step: 0,
+      requirements: (dt) => {
+        return !!dt.user.username
+      },
       state: "continue",
       title: "Step 1",
       Component: () => <div className="flex-1 bg-green-500" />
@@ -15,6 +26,9 @@ const StepByStep = () => {
     {
       id: 0,
       step: 0,
+      requirements: (dt) => {
+        return !!dt.order.orderId
+      },
       state: "complate",
       title: "Step 2",
       Component: () => <div className="flex-1 bg-red-500" />
@@ -22,6 +36,9 @@ const StepByStep = () => {
     {
       id: 0,
       step: 0,
+      requirements: (dt) => {
+        return !!dt.order?.complate
+      },
       state: "none",
       title: "Step 3",
       Component: () => <div className="flex-1 bg-orange-500" />
@@ -44,7 +61,7 @@ const StepByStep = () => {
         {steps.map((step, index) => (
           <div
           onClick={() => setCurrentStep(index)}
-          className={twMerge(getColor(step.state), "p-3 font-semibold rounded hover:opacity-75 transition-all duration-200 cursor-pointer")}>
+          className={twMerge(getColor(step.state), "p-3 font-semibold rounded hover:opacity-75 transition-all duration-200 cursor-pointer", step.requirements(data) && "opacity-25 pointer-events-none")}>
             {step.title}
           </div>
         ))}
