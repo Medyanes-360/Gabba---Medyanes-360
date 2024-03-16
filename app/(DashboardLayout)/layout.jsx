9a2b8241f05e5af6e16c0f0ac9f61bedc574d3b2
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-03-16 14:42:43 +0300

    dashboard isloading context created.

--- app/(DashboardLayout)/layout.jsx
+++ app/(DashboardLayout)/layout.jsx
@@ -1,10 +1,17 @@
 'use client';
+import React, { useState, createContext, useContext } from 'react';
 import Sidebar from '@/components/dashboard/Sidebar';
-import { useParams } from 'next/navigation'
+import { useParams } from 'next/navigation';
 import UserIcon from '@/assets/icons/UserIcon';
+import LoadingScreen from '@/components/other/dashboardLoading';
+
+const LoadingContext = createContext();
 
 const MainLayout = ({ children }) => {
-  const { id } = useParams()
+  // Context oluşturuluyor
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { id } = useParams();
 
   const buttons = [
     {
@@ -68,20 +75,26 @@ const MainLayout = ({ children }) => {
     },
   ];
 
-
   return (
-    <div className='flex h-screen w-full overflow-hidden bg-background pb-4'>
-      <Sidebar buttons={buttons} />
+    <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+      <div className='flex h-screen w-full overflow-hidden bg-background pb-4 relative'>
+        <LoadingScreen isloading={isLoading} />
+        <Sidebar buttons={buttons} />
+        <div className='flex flex-1 w-full flex-col h-full px-4 overflow-hidden gap-2'>
+          {/* <Navbar /> */}
 
-      <div className='flex flex-1 w-full flex-col h-full px-4 overflow-hidden gap-2'>
-        {/* <Navbar /> */}
-
-        <div className='flex flex-1 h-full overflow-auto bg-background rounded-lg'>
-          <div className='py-6 w-full pl-6 md:pl-0'>{children}</div>
+          <div className='flex flex-1 h-full overflow-auto bg-background rounded-lg'>
+            <div className='py-6 w-full pl-6 md:pl-0'>{children}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </LoadingContext.Provider>
   );
+};
+
+// Alt bileşenlerde LoadingContext'i kullanmak için bir custom hook oluşturuluyor
+export const useLoadingContext = () => {
+  return useContext(LoadingContext);
 };
 
 export default MainLayout;
