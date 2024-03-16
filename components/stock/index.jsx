b930aeaa7e95c55cfa917c371dc7a549157a2315
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-03-16 14:47:32 +0300

    loading component updated, states updated.

--- components/stock/index.jsx
+++ components/stock/index.jsx
@@ -1,29 +1,27 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
-import LoadingScreen from '@/components/other/loading';
+import { toast } from 'react-toastify';
 import ListProducts from './listProducts';
 import { getAPI } from '@/services/fetchAPI';
 import FinancialManagementCalculate from '@/functions/others/financialManagementCalculate';
-import { BiFilterAlt } from 'react-icons/bi';
-import { FaFileInvoice } from 'react-icons/fa';
+import { useLoadingContext } from '@/app/(DashboardLayout)/layout';
 
 const Stock = () => {
   const [stores, setStores] = useState([]);
-  const [showOrderOffer, setShowOrderOffer] = useState(false);
   const [hiddenBasketBar, setHiddenBasketBar] = useState(false);
-  const [isloading, setIsloading] = useState(false);
   const [products, setProducts] = useState([]);
   const [productFeatures, setProductFeatures] = useState([]);
-  const [showBasketOffer, setShowBasketOffer] = useState(false);
   const [allFeatureValues, setAllFeatureValues] = useState([]);
 
   // Sepetteki ürünleri tuttuğumuz state.
   const [basketData, setBasketData] = useState([]);
   // Sepetteki ürünlerin stok değerlerini tuttuğumuz state.
+
+  const { isLoading, setIsLoading } = useLoadingContext();
+
   const getData = async () => {
     try {
-      setIsloading(true);
+      setIsLoading(true);
       const response = await getAPI('/createProduct/createProduct');
       if (!response) {
         throw new Error('Veri çekilemedi 2');
@@ -47,7 +45,7 @@ const Stock = () => {
       );
       setProducts(response.data.createProducts);
       setProductFeatures(response.data.productFeatures);
-      setIsloading(false);
+      setIsLoading(false);
     } catch (error) {
       toast.error(error.message);
     }
@@ -56,7 +54,7 @@ const Stock = () => {
   async function getAllBasketData() {
     const response = getAPI('/createOffer/basket');
     const [dataResult] = await Promise.all([response]);
-    setIsloading(false);
+    setIsLoading(false);
     setBasketData(dataResult.data);
   }
 
@@ -74,50 +72,12 @@ const Stock = () => {
 
   return (
     <>
-      {isloading && <LoadingScreen isloading={isloading} />}
-      <ToastContainer
-        position='top-right'
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme='dark'
-      />
-
-      <div
-        className={`${
-          hiddenBasketBar ? 'hidden' : 'flex'
-        } flex-col md:flex-row p-2 lg:p-2 lg:px-10 w-full justify-between mb-4 items-center shadow-lg bg-background gap-2 pr-4`}
-      >
-        {/* Filtreleme ve Teklifler Butonu */}
-        <div className='flex justify-center item-center flex-col md:flex-row gap-2 px-4 my-2'>
-          <button className='bg-green-500 p-4 text-white rounded lg:text-lg flex flex-row gap-2 flex-nowrap hover:cursor-pointer hover:scale-105 transition-all mt-2 lg:mt-0'>
-            <BiFilterAlt size={25} />
-            Filtrele
-          </button>
-          <button
-            onClick={() => {
-              setShowOrderOffer(true);
-              setShowBasketOffer(false);
-            }}
-            className='bg-purple-600 p-4 text-white rounded lg:text-lg flex flex-row gap-2 flex-nowrap hover:cursor-pointer hover:scale-105 transition-all mt-2 lg:mt-0'
-          >
-            <FaFileInvoice size={25} />
-            Stoklar
-          </button>
-        </div>
-      </div>
-
       <ListProducts
         getData={getData}
         getAllBasketData={getAllBasketData}
         toast={toast}
-        isloading={isloading}
-        setIsloading={setIsloading}
+        isloading={isLoading}
+        setIsloading={setIsLoading}
         products={products}
         productFeatures={productFeatures}
         setHiddenBasketBar={setHiddenBasketBar}
