13f1ba6b9619be7dd9f61f2d93fa48e8b6e247fb
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-04-01 08:20:37 +0300

    customer post added.

--- pages/api/customer/index.js
+++ pages/api/customer/index.js
@@ -1,7 +1,43 @@
-import { getAllData } from '@/services/serviceOperations';
+import {
+  getAllData,
+  getDataByUnique,
+  createNewData,
+} from '@/services/serviceOperations';
 
 const handler = async (req, res) => {
   try {
+    if (req.method === 'POST') {
+      const data = req.body;
+
+      //! DATA İÇERİSİNDE VERİLER GELİYOR MU KONTROL EDİLECEK.
+
+      // email adresi veritabanında kayıtlı mı kontrol ediyoruz.
+      const findMailAddress = await getDataByUnique('Customer', {
+        mailAddress: data.mailAddress,
+      });
+      // Telefon Numarası veritabanında kayıtlı mı kontrol ediyoruz.
+      const findPhoneNumber = await getDataByUnique('Customer', {
+        phoneNumber: data.phoneNumber,
+      });
+
+      // Eğer veri tabanında daha önce kayıtlı değilse, kayıt ekliyoruz.
+      if (findMailAddress == null && findPhoneNumber == null) {
+        // Müşteri bilgisini veritabanına kaydediyoruz.
+        const customerData = await createNewData('Customer', data);
+        if (customerData) {
+          return res.status(200).json({
+            status: 'success',
+            message: 'Müşterinin bilgileri başarılı bir şekilde kaydedildi!',
+          });
+        } else {
+          throw new Error('Müşterinin bilgileri kayıt edilirken hata oluştu!');
+        }
+      } else {
+        throw new Error(
+          'Bu müşterinin bilgileri daha önce zaten kayıt edildi.'
+        );
+      }
+    }
     if (req.method === 'GET') {
       const customerData = await getAllData('Customer');
       if (!customerData || customerData.error) {
