7d04270e57b2762653a50f407dc48ac5eea35755
yusuf <ysfyldz0077@gmail.com>
2024-04-15 22:27:46 +0300

    step by step

---
+++ app/(StepsLayout)/stepbystep/[id]/1.1/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import SendDocument from "@/components/stepbystep/SendDocument"
+import { useParams } from "next/navigation";
+
+const StepPage = () => {
+  const { id } = useParams();
+
+  return <SendDocument lang={"tr"} id={id} />;
+};
+
+export default StepPage;
--- pages/api/mail/index.js
+++ pages/api/mail/index.js
@@ -1,29 +1,29 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
-    }
-})
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 export const mailOptions = {
-    from: process.env.EMAIL,
-}
+  from: process.env.EMAIL,
+};
 
 const handler = async (req, res) => {
-    //getServerSession:  Kullanıcının oturum açıp açmadığını kontrol eder. Eğer açılmışsa session değişkenine atar.
+  //getServerSession:  Kullanıcının oturum açıp açmadığını kontrol eder. Eğer açılmışsa session değişkenine atar.
 
-    if (req.method === 'POST') {
-        try {
-            const { email, url } = req.body;
+  if (req.method === "POST") {
+    try {
+      const { email, url } = req.body;
 
-            //mail gönderme işlemi
-            transporter.sendMail({
-                ...mailOptions,
-                subject: `TEST`,
-                html: `
+      //mail gönderme işlemi
+      transporter.sendMail({
+        ...mailOptions,
+        subject: `TEST`,
+        html: `
                 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,21 +32,30 @@ const handler = async (req, res) => {
     <title>Gabba - Sipariş fişiniz</title>
 </head>
 <body>
-<iframe src="https://gabba-medyanes-360.vercel.app/document?id=225329YUSUAJ94OH&lang=tr" frameborder="0"></iframe>
+<p>${url}</p>
 </body>
 </html>
                 `,
-                to: email,
-            })
+        to: email,
+      });
 
-            return res.status(200).json({ status: "success", message: "Şifre sıfırlama bağlantısı mail adresinize gönderildi." });
-        } catch (error) {
-            return res.status(500).json({ status: "error", message: error.message });
-        }
+      return res
+        .status(200)
+        .json({
+          status: "success",
+          message: "Şifre sıfırlama bağlantısı mail adresinize gönderildi.",
+        });
+    } catch (error) {
+      return res.status(500).json({ status: "error", message: error.message });
     }
-    else {
-        return res.status(405).json({ status: "error", message: "hatalı bir istek gerçekleştirdiniz." });
-    }
+  } else {
+    return res
+      .status(405)
+      .json({
+        status: "error",
+        message: "hatalı bir istek gerçekleştirdiniz.",
+      });
+  }
 };
 
 export default handler;
