a5a32e189b600cb85356dc9c1e9275b4071c63cc
Yusuf Yıldız <yyilidz518@gmail.com>
2024-05-05 16:02:58 +0300

    c

--- app/(StepsLayout)/layout.jsx
+++ app/(StepsLayout)/layout.jsx
@@ -92,7 +92,47 @@ const MainLayout = ({ children }) => {
       <div className='flex flex-1 w-full flex-col h-full px-4 overflow-hidden gap-2'>
         {/* <Navbar /> */}
 
-        <div className='flex flex-1 h-full overflow-auto bg-background rounded-lg'>
+        <div className='flex flex-col flex-1 h-full overflow-auto bg-background rounded-lg'>
+          <div className="w-full flex py-2 px-4">
+            <ul className='divide-x flex items-center gap-4 divide-gray-700 text-gray-300'>
+              <li className=''>
+                Oluşturma Tarihi:{' '}
+                {item.Orders.map(
+                    (orders, index) =>
+                        index == 0 &&
+                        orders.createdAt
+                            .split('T')[0]
+                            .split('-')
+                            .reverse()
+                            .join('.')
+                )}
+              </li>
+              <li className=''>
+                Müşteri İsmi: {item.Müşteri[0]?.name}{' '}
+                {item.Müşteri[0]?.surname}
+              </li>
+              <li className=''>
+                Firma İsmi: {item.Müşteri[0]?.company_name}
+              </li>
+              <li className=''>Ürün Adedi: {item.Orders.length}</li>
+              <li className=''>
+                Fiyat:{' '}
+                {item.Orders.reduce((total, order) => {
+                  return (
+                      total +
+                      (order.productPrice + order.productFeaturePrice) *
+                      order.stock
+                  );
+                }, 0)}
+              </li>
+              <li>
+                Durum:{' '}
+                <span className='text-xs font-medium rounded-full bg-blue-900 text-blue-300'>
+                        {item.Orders[0].ordersStatus}
+                      </span>
+              </li>
+            </ul>
+          </div>
           <div className='py-6 w-full pl-6 md:pl-0'>{children}</div>
         </div>
       </div>
