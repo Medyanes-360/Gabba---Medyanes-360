2d5bbe65ade972bb2efc63bc670423db54997f4d
Yusuf Yıldız <yyilidz518@gmail.com>
2024-05-05 15:49:31 +0300

    x

--- components/createOffer/orderOffer/PrintOrder/ButtonPrint.jsx
+++ components/createOffer/orderOffer/PrintOrder/ButtonPrint.jsx
@@ -343,191 +343,185 @@ const InvoiceJustButton = ({ data, lang, title }) => {
         {/* Header */}
         <header className='flex items-center justify-end mb-6 px-4'>
           <Image
-            src='/Logo.png'
-            width={300}
-            height={300}
-            alt=''
-            className='mr-auto'
+              src='/Logo.png'
+              width={300}
+              height={300}
+              alt=''
+              className='mr-auto'
           />
 
           <div className='flex flex-col gap-1'>
-            <span className='text-[#363B46] text-[19.125pt] font-bold'>
+            <span className='text-[#000] text-[19.125pt] font-bold'>
               {details.musteri.name}
             </span>
-            <span className='text-[13.5pt] text-[#647680] font-bold'>
+            <span className='text-[13.5pt] text-[#000] font-bold'>
               {details.musteri.phone}
             </span>
-            <span className='text-[13.5pt] text-[#647680] font-bold'>
+            <span className='text-[13.5pt] text-[#000] font-bold'>
               {details.musteri.email}
             </span>
-            <span className='text-[13.5pt] text-[#647680] font-bold'>
+            <span className='text-[13.5pt] text-[#000] font-bold'>
               {details.musteri.adress}
             </span>
           </div>
         </header>
 
         <table className='w-full break-inside-auto'>
-          <thead className='h-[50px] w-full bg-[#FFC90B]'>
-            <tr>
-              <th>
-                <span className='text-[10pt] text-[#363B46] font-bold'>
-                  {langs.invoice[lang]}#
+          <thead className='h-[50px] w-full'>
+          <tr>
+            <th>
+                <span className='text-[10pt] text-[#000] font-bold'>
+                  {langs.teklifNo[lang]}:
                 </span>
-              </th>
-              <th>
-                <span className='text-[10pt] text-[#000] font-bold'>45489</span>
-              </th>
-              <th>
-                <div className='flex items-center justify-around'>
-                  <div className='flex items-center justify-between gap-2'>
-                    <span className='text-[10pt] text-[#363B46] font-bold'>
-                      {langs.teklifNo[lang]}:
-                    </span>
-                    <span className='text-[10pt] text-[#000] font-bold'>
-                      {details.order_no}
-                    </span>
-                  </div>
-
-                  <div className='flex items-center justify-between gap-2'>
-                    <span className='text-[10pt] text-[#363B46] font-bold'>
-                      {langs.date[lang]}:
-                    </span>
-                    <span className='text-[10pt] text-[#000] font-bold'>
-                      12 / 04 / 2023
-                    </span>
-                  </div>
-                </div>
-              </th>
-              <th />
-              <th />
-              <th></th>
-            </tr>
+            </th>
+            <th>
+                <span className='text-[10pt] text-[#000] font-bold'>
+                  {details.order_no}
+                </span>
+            </th>
+            <th />
+            <th />
+            <th><span className='text-[10pt] text-[#000] font-bold'>
+                {langs.date[lang]}:
+              </span></th>
+            <th><span className='text-[10pt] text-[#000] font-bold'>
+                {data.Orders.map(
+                    (orders, index) =>
+                        index == 0 &&
+                        orders.createdAt
+                            .split('T')[0]
+                            .split('-')
+                            .reverse()
+                            .join('.')
+                )}
+              </span></th>
+          </tr>
           </thead>
 
-          <thead className='bg-[#363B46] [&_tr_th]:text-center  [&_tr_th]:text-white'>
-            <tr className='h-8'>
-              <th className='px-1 w-fit !font-serif'>{langs.order[lang]}</th>
-              <th className='px-1 w-min !font-serif'>{langs.image[lang]}</th>
-              <th className='!font-serif'>{langs.productFeatures[lang]}</th>
-              <th className='!font-serif'>{langs.price[lang]}</th>
-              <th className='!font-serif'>{langs.quantity[lang]}</th>
-              <th className='!font-serif'>{langs.total[lang]}</th>
-            </tr>
+          <thead className='border border-black [&_tr_th]:text-center  [&_tr_th]:text-black'>
+          <tr className='h-8'>
+            <th className='px-1 w-fit !font-serif'>{langs.order[lang]}</th>
+            <th className='px-1 w-min !font-serif'>{langs.image[lang]}</th>
+            <th className='!font-serif'>{langs.productFeatures[lang]}</th>
+            <th className='!font-serif'>{langs.price[lang]}</th>
+            <th className='!font-serif'>{langs.quantity[lang]}</th>
+            <th className='!font-serif'>{langs.total[lang]}</th>
+          </tr>
           </thead>
-          <tbody className='[&_tr_td]:p-[6px] [&_tr_td]:text-center [&_tr_th]:text-[#363B46]'>
-            {details?.products?.map((product, idx) => (
+          <tbody className='[&_tr_td]:p-[6px] [&_tr_td]:text-center [&_tr_th]:text-[#000]'>
+          {details?.products?.map((product, idx) => (
               <tr
-                key={idx}
-                className='even:bg-[#F2F2F2] bg-white break-inside-avoid'
+                  key={idx}
+                  className='even:bg-[#F2F2F2] bg-white break-inside-avoid'
               >
-                <td className='border-l-2 border-r-2 border-b-2 border-dashed border-[#363B46] text-[13.5pt] text-[#363B46] font-bold'>
+                <td className='border border-black text-[13.5pt] text-[#000] font-bold'>
                   {idx + 1}
                 </td>
-                <td className='border-r-2 border-b-2 border-dashed border-[#363B46]'>
+                <td className='border border-black'>
                   {product?.image ? (
-                    <Image
-                      src={product?.image}
-                      height={75}
-                      alt=''
-                      className='object-contain m-auto'
-                    />
+                      <Image
+                          src={product?.image}
+                          height={75}
+                          alt=''
+                          className='object-contain m-auto'
+                      />
                   ) : (
-                    <Image
-                      src='/no-image2.png'
-                      width={200}
-                      height={50}
-                      alt=''
-                      className='object-contain m-auto max-h-16'
-                    />
+                      <Image
+                          src='/no-image2.png'
+                          width={200}
+                          height={50}
+                          alt=''
+                          className='object-contain m-auto max-h-16'
+                      />
                   )}
                 </td>
-                <td className='border-r-2 border-b-2 border-dashed border-[#363B46] overflow-hidden'>
+                <td className='border border-black overflow-hidden'>
                   <div className='!max-h-fit overflow-hidden gap-2 flex flex-wrap'>
-                    <span className='px-2.5 py-1 bg-green-600 text-[9pt] text-white rounded-full w-fit'>
+                    <span className='px-2.5 py-1 text-[9pt] text-black border border-black rounded-md font-semibold rounded-full w-fit'>
                       {product.name}
                     </span>
 
-                    <span className='px-2.5 py-1 bg-yellow-500 text-[9pt] text-white rounded-full w-fit'>
+                    <span className='px-2.5 py-1 text-[9pt] text-black border border-black rounded-md font-semibold rounded-full w-fit'>
                       {product.info}
                     </span>
 
                     {product?.extras?.map((feature, idx) => (
-                      <div
-                        key={'feature-' + idx}
-                        className='px-2 py-1 bg-[#363B46] text-center align-middle flex gap-1 [&_span]:text-white rounded-full [&_span]:text-[10pt] items-center'
-                      >
-                        <span>{feature.value}</span>
-                      </div>
+                        <div
+                            key={'feature-' + idx}
+                            className='px-2 py-1 text-center align-middle flex gap-1 [&_span]:text-black border border-black rounded-full [&_span]:text-[10pt] items-center'
+                        >
+                          <span>{feature.value}</span>
+                        </div>
                     ))}
 
                     {product.note && product.note !== '' && (
-                      <div className='px-2 py-1 bg-slate-600 flex gap-1 [&_span]:text-white rounded-md [&_span]:text-[10pt] items-center w-full'>
-                        <span>{product.note}</span>
-                      </div>
+                        <div className='px-2 py-1 flex gap-1 [&_span]:text-black border border-black rounded-md [&_span]:text-[10pt] items-center w-full'>
+                          <span>{product.note}</span>
+                        </div>
                     )}
                   </div>
                 </td>
-                <td className='border-r-2 border-b-2 border-dashed border-[#363B46] whitespace-nowrap '>
+                <td className='border border-black whitespace-nowrap '>
                   {formatCurrency(product.price)} грн
                 </td>
-                <td className='border-r-2 border-b-2 border-dashed border-[#363B46] whitespace-nowrap'>
+                <td className='border border-black whitespace-nowrap'>
                   {product.quantity} грн
                 </td>
-                <td className='border-r-2 border-b-2 border-dashed border-[#363B46] whitespace-nowrap'>
+                <td className='border border-black whitespace-nowrap'>
                   {formatCurrency(product.quantity * product.price)} грн
                 </td>
               </tr>
-            ))}
+          ))}
           </tbody>
         </table>
 
-  {/* Footer */ }
-  <footer className='flex justify-between break-inside-avoid mx-2'>
-    <div className='mt-[24px] flex flex-col h-full'>
-      <span className='text-[#363B46] text-[15pt] font-bold'>
-        {langs.firmaBilgileri[lang]}
-      </span>
-      <span className='text-[10pt] text-[#647680] font-bold'>
-        Фізична особа-підприємець Дурал Онур код за ЄДРПОУ 2896224270{' '}
-      </span>
-      <span className='text-[10pt] text-[#647680] font-bold'>
-        UA043052990000026008040126820, Банк АТ КБ "ПРИВАТБАНК", МФО 305299{' '}
-      </span>
-      <span className='text-[10pt] text-[#647680] font-bold'>
-        Україна, 76006, Івано-Франківська обл., м.Івано-Франківськ,
-        вул.Вовчинецька, будинок № 200, кв.7
-      </span>
-    </div>
+        {/* Footer */}
+        <footer className='flex justify-between break-inside-avoid mx-2'>
+          <div className='mt-[24px] flex flex-col h-full'>
+            <span className='text-[#000] text-[15pt] font-bold'>
+              {langs.firmaBilgileri[lang]}
+            </span>
+            <span className='text-[10pt] text-[#000] font-bold'>
+              Фізична особа-підприємець Дурал Онур код за ЄДРПОУ 2896224270{' '}
+            </span>
+            <span className='text-[10pt] text-[#000] font-bold'>
+              UA043052990000026008040126820, Банк АТ КБ "ПРИВАТБАНК", МФО 305299{' '}
+            </span>
+            <span className='text-[10pt] text-[#000] font-bold'>
+              Україна, 76006, Івано-Франківська обл., м.Івано-Франківськ,
+              вул.Вовчинецька, будинок № 200, кв.7
+            </span>
+          </div>
 
-    <div className='flex flex-col items-end gap-2 mt-[24px]'>
-      <div className='flex items-center gap-6 px-1.5 w-[300px]'>
-        <span className='text-[#363B46] text-[13pt] font-bold'>
-          {langs.total[lang]} :
-        </span>
-        <p className='ml-auto text-[#363B46] text-[12pt] font-bold'>
-          {total.total} грн
-        </p>
-      </div>
+          <div className='flex flex-col items-end gap-2 mt-[24px]'>
+            <div className='flex items-center gap-6 px-1.5 w-[300px]'>
+              <span className='text-[#000] text-[13pt] font-bold'>
+                {langs.total[lang]} :
+              </span>
+              <p className='ml-auto text-[#000] text-[12pt] font-bold'>
+                {total.total} грн
+              </p>
+            </div>
 
-      <div className='flex items-center gap-6 px-1.5 w-[300px]'>
-        <span className='text-[#363B46] text-[13pt] font-bold'>
-          {langs.vergi[lang]} :
-        </span>
-        <p className='ml-auto text-[#363B46] text-[12pt] font-bold'>
-          {total.tax} грн
-        </p>
-      </div>
+            <div className='flex items-center gap-6 px-1.5 w-[300px]'>
+              <span className='text-[#000] text-[13pt] font-bold'>
+                {langs.vergi[lang]} :
+              </span>
+              <p className='ml-auto text-[#000] text-[12pt] font-bold'>
+                {total.tax} грн
+              </p>
+            </div>
 
-      <div className='flex items-center gap-6 w-[300px] bg-[#FFC90B] p-1.5 rounded-sm'>
-        <span className='text-[#363B46] text-[13pt] font-bold'>
-          {langs.genelToplam[lang]} :
-        </span>
-        <p className='ml-auto text-[#363B46] text-[12pt] font-bold'>
-          {total.grandTotal} грн
-        </p>
-      </div>
-    </div>
-  </footer>
+            <div className='flex items-center gap-6 w-[300px] border-b border-b-black p-1.5 rounded-sm'>
+              <span className='text-[#000] text-[13pt] font-bold'>
+                {langs.genelToplam[lang]} :
+              </span>
+              <p className='ml-auto text-[#000] text-[12pt] font-bold'>
+                {total.grandTotal} грн
+              </p>
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
   );
