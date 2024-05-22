23e6f43670a1aed4b301995db8643ebe1e365048
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-05-23 00:18:17 +0300

    Yapılacaklar 1 done.

--- app/(StepsLayout)/stepbystep/[id]/1.2/page.jsx
+++ app/(StepsLayout)/stepbystep/[id]/1.2/page.jsx
@@ -1,23 +1,28 @@
-"use client";
+'use client';
 
-import SendDocument from "@/components/stepbystep/SendDocument"
-import { useParams } from "next/navigation";
-import {Input} from "@/components/ui/input";
-import {Label} from "@/components/ui/label";
-import {Button} from "@/components/ui/button";
+import SendDocument from '@/components/stepbystep/SendDocument';
+import { useParams } from 'next/navigation';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
 
 const StepPage = () => {
   const { id } = useParams();
 
-  return <div className={'h-full w-full flex items-center justify-center'}>
-    <div className="max-w-sm w-full flex flex-col gap-4">
-      <Label>Cariye İşlenecek Ön Ödeme Miktarı</Label>
-      <div className="flex gap-2 items-center">
-        <Input type={'number'} placeholder={'0'}/>
+  return (
+    <div className={'h-full w-full flex items-center justify-center'}>
+      <div className='max-w-sm w-full flex flex-col gap-4'>
+        <Label className={'text-xs'}>
+          Açıklama: Müşteriden alınan ön ödeme miktarını burada giriniz.
+        </Label>
+        <Label>Cariye İşlenecek Ön Ödeme Miktarı</Label>
+        <div className='flex gap-2 items-center'>
+          <Input type={'number'} placeholder={'0'} />
+        </div>
+        <Button>Cariye işle</Button>
       </div>
-      <Button>Cariye işle</Button>
     </div>
-  </div>;
+  );
 };
 
 export default StepPage;
