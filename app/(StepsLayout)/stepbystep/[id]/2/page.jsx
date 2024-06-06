c69b1a39d40543b33f7e8efe111a42ad917261a0
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-06-06 11:44:14 +0300

    Tedarikçi seçimi eklendl

--- app/(StepsLayout)/stepbystep/[id]/2/page.jsx
+++ app/(StepsLayout)/stepbystep/[id]/2/page.jsx
@@ -1,70 +1,169 @@
-'use client'
+'use client';
 
-import {Label} from "@/components/ui/label";
+import { Label } from '@/components/ui/label';
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
-import {Button} from "@/components/ui/button";
-import {useEffect, useState} from "react";
-import {getAPI} from "@/services/fetchAPI";
-import {Input} from "@/components/ui/input";
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Button } from '@/components/ui/button';
+import { useEffect, useState } from 'react';
+import { getAPI, postAPI } from '@/services/fetchAPI';
+import { Input } from '@/components/ui/input';
+import { Formik, Form } from 'formik';
+import { toast } from 'react-toastify';
+import {
+  useLoadingContext,
+  useStepByStepDataContext,
+} from '@/app/(StepsLayout)/layout';
+import { useParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 const StepPage = () => {
-    const [tedarikci, setTedarikci] = useState('')
-    const [data, setData] = useState([])
+  const { id } = useParams();
+  const router = useRouter();
 
-    const getTedarikciId = () => {
-        const filtered = data.filter((dt) => dt.firma === tedarikci)
+  const [tedarikci, setTedarikci] = useState('');
+  const [initialValues, setInitialValues] = useState({
+    tedarikciAciklama: '',
+    tedarikciId: 0,
+    step: 3,
+    stepName: 'Tedarikçi Yükleme Tarihi',
+  });
+  const [data, setData] = useState([]);
+  const { isLoading, setIsLoading } = useLoadingContext();
+  const { stepByStepData, setStepByStepData } = useStepByStepDataContext();
 
-        if (filtered.length === 0) {
-            return null
+  const getTedarikciId = () => {
+    const filtered = data.filter((dt) => dt.firma === tedarikci);
+
+    if (filtered.length === 0) {
+      return null;
+    }
+
+    return filtered[0].id;
+  };
+
+  const getData = async () => {
+    const dt = await getAPI('/suplier');
+    setData(dt.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const stepData = stepByStepData.find((data) => data.orderId === id);
+    if (stepByStepData?.length > 0) {
+      const filtered = data.filter((dt) => dt.id === stepData.tedarikciId);
+      if (filtered.length > 0) {
+        setTedarikci(filtered[0].firma);
+      }
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (stepByStepData?.length > 0) {
+      const stepData = stepByStepData.find((data) => data.orderId === id);
+      if (stepData) {
+        setInitialValues({
+          onOdemeMiktari: stepData.onOdemeMiktari,
+          orderId: id,
+          step: 3,
+          stepName: 'Tedarikçi Yükleme Tarihi',
+          tedarikciAciklama: stepData.tedarikciAciklama,
+          tedarikciId: stepData.tedarikciId,
+        });
+        const filtered = data.filter((dt) => dt.id === stepData.tedarikciId);
+        if (filtered.length > 0) {
+          setTedarikci(filtered[0].firma);
         }
-
-        return filtered[0].id
+      }
     }
-
-    const getData = async () => {
-        const dt = await getAPI('/suplier')
-        setData(dt.data)
-    }
-
-    useEffect(() => {
-        getData()
-    }, [])
+  }, [stepByStepData]);
 
   return (
-      <div className={'h-full w-full flex items-center justify-center'}>
-          <div className="max-w-sm w-full flex flex-col gap-4">
-              <Label>Lütfen bir tedarikçi seçiniz.</Label>
-              <Select disabled={data.length === 0} onValueChange={(value) => setTedarikci(value)}>
-                  <SelectTrigger className="w-full">
-                      <SelectValue placeholder="Tedarikçiler" />
-                  </SelectTrigger>
-                  <SelectContent>
-                      <SelectGroup>
-                          <SelectLabel>Tedarikçi Listesi</SelectLabel>
-                          {data.map((item, i) => {
-                              console.log(item)
-                              return <SelectItem key={item.id} value={item.firma}>{item.firma}</SelectItem>
-                          })}
-                      </SelectGroup>
-                  </SelectContent>
-              </Select>
+    <div className={'h-full w-full flex items-center justify-center'}>
+      <Formik
+        initialValues={initialValues}
+        enableReinitialize={true}
+        onSubmit={async (values) => {
+          if (values.tedarikciAciklama.length === 0)
+            return toast.error(
+              'Lütfen tedarikçiye göndereceğiniz açıklamayı boş bırakmayınız!'
+            );
 
-              <Input type={"text"} className={"w-full"} placeholder={"Tedarikciye Gonderilecek Aciklama"}/>
+          const tedarikciId = getTedarikciId();
+          values.tedarikciId = tedarikciId;
+          if (values.tedarikciId === 0 || values.tedarikciId === null)
+            return toast.error(
+              'Tedarikçi seçilirken bir hata gerçekleşti. Lütfen tedarikçi seçtiğinizden emin olunuz!'
+            );
 
-              <Button onClick={() => {
-                  console.log(getTedarikciId())
-              }}>Tedarikciye verileri gonder</Button>
-          </div>
-      </div>
-  )
-}
+          values.id = id;
 
-export default StepPage
+          setIsLoading(true);
+          const response = await postAPI('/stepByStep/tedarikci', values);
+          if (response.error || !response) {
+            toast.error(response.message);
+            return setIsLoading(false);
+          } else {
+            toast.success(response.message);
+            const response2 = await getAPI(`/stepByStep?orderId=${id}`);
+            setStepByStepData(response2.data);
+            router.push(`/stepbystep/${id}/3`);
+            return setIsLoading(false);
+          }
+        }}
+      >
+        {(props) => (
+          <Form
+            className='max-w-sm w-full flex flex-col gap-4'
+            onSubmit={props.handleSubmit}
+          >
+            <Label>Lütfen bir tedarikçi seçiniz.</Label>
+            <Select
+              disabled={data.length === 0}
+              onValueChange={(value) => setTedarikci(value)}
+              value={tedarikci}
+            >
+              <SelectTrigger className='w-full'>
+                <SelectValue placeholder='Tedarikçiler' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Tedarikçi Listesi</SelectLabel>
+                  {data.map((item, i) => {
+                    return (
+                      <SelectItem key={item.id} value={item.firma}>
+                        {item.firma}
+                      </SelectItem>
+                    );
+                  })}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
+            <Input
+              type='text'
+              name='tedarikciAciklama'
+              onChange={props.handleChange}
+              className='w-full'
+              placeholder='Tedarikçiye Gönderilecek Açıklama'
+              value={props.values.tedarikciAciklama}
+            />
+
+            <Button type='submit'>Tedarikçiye verileri gönder</Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default StepPage;
