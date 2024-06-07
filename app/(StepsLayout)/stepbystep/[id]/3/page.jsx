e50a9c4ce0cf95bc60ee4b0a3e25bef49cf8ea39
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-06-08 00:57:29 +0300

    tedarikçi yükleme tarihi eklendi

--- app/(StepsLayout)/stepbystep/[id]/3/page.jsx
+++ app/(StepsLayout)/stepbystep/[id]/3/page.jsx
@@ -1,83 +1,157 @@
-'use client'
+'use client';
 
-import {useEffect, useState} from "react";
-import {getAPI} from "@/services/fetchAPI";
-import {useParams} from "next/navigation";
-import {Label} from "@/components/ui/label";
-import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
-import {Button} from "@/components/ui/button";
-import {cn} from "@/lib/table/utils";
-import {CalendarIcon} from "lucide-react";
-import {format} from "date-fns";
-import {Calendar} from "@/components/ui/calendar";
+import { useEffect, useState } from 'react';
+import { getAPI, postAPI } from '@/services/fetchAPI';
+import { useParams } from 'next/navigation';
+import { Label } from '@/components/ui/label';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/table/utils';
+import { CalendarIcon } from 'lucide-react';
+import { format } from 'date-fns';
+import { Calendar } from '@/components/ui/calendar';
+import {
+  useLoadingContext,
+  useStepByStepDataContext,
+} from '@/app/(StepsLayout)/layout';
+import { Formik, Form } from 'formik';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
 
 const StepPage = () => {
-    const { id } = useParams();
-    const [data, setData] = useState([])
-    const [date, setDate] = useState()
+  const { id } = useParams();
+  const router = useRouter();
 
-    const initializeDateState = (length) => {
-        const initialDates = new Array(length).fill(undefined);
-        setDate(initialDates);
-    };
+  const [data, setData] = useState([]);
+  const [date, setDate] = useState();
+  const { loading, setIsLoading } = useLoadingContext();
+  const { stepByStepData, setStepByStepData } = useStepByStepDataContext();
 
-    const getAllOrderData = async () => {
-        const response = await getAPI("/createOrder/order");
-        const filtered = response.data?.filter((fl) => fl.orderCode === id)[0];
-        setData(filtered);
-        initializeDateState(filtered?.Ürünler?.length || 0);
-    };
+  const initializeDateState = (length) => {
+    const initialDates = new Array(length).fill(undefined);
+    setDate(initialDates);
+  };
 
-    useEffect(() => {
-        getAllOrderData();
-    }, []);
+  const getAllOrderData = async () => {
+    setIsLoading(true);
+    const response = await getAPI('/createOrder/order');
+    const filtered = response.data?.filter((fl) => fl.orderCode === id)[0];
+    setData(filtered);
+    initializeDateState(filtered?.Ürünler?.length || 0);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getAllOrderData();
+  }, []);
 
   return (
     <div className={'flex h-full w-full items-center justify-center'}>
-        <div className="max-w-sm w-full flex flex-col gap-2">
-            <Label className={"text-xs"}>
-                Açıklama : Bu bölümde siparişlerin tedarikçi tarafından yüklendiği tarih bilgisi girilir, buna göre tedarikçi tarafından ürünlerin birim bazında ne zaman yüklendiği belirlenir.
+      <Formik
+        initialValues={{
+          dates: [],
+          step: 5,
+          stepName: 'Ürün Maliyeti ve Çıkış Tarihi',
+        }}
+        onSubmit={async (values) => {
+          const response = await postAPI(
+            '/stepByStep/tedarikciYuklemeTarihi',
+            values
+          );
+          if (response.error || !response) {
+            toast.error(response.message);
+            return setIsLoading(false);
+          } else {
+            toast.success(response.message);
+            const response2 = await getAPI(`/stepByStep?orderCode=${id}`);
+            setStepByStepData(response2.data);
+            router.push(`/stepbystep/${id}/5`);
+            return setIsLoading(false);
+          }
+        }}
+      >
+        {(props) => (
+          <Form
+            className='max-w-sm w-full flex flex-col gap-2'
+            onSubmit={props.handleSubmit}
+          >
+            <Label className={'text-xs'}>
+              Açıklama : Bu bölümde siparişlerin tedarikçi tarafından yüklendiği
+              tarih bilgisi girilir, buna göre tedarikçi tarafından ürünlerin
+              birim bazında ne zaman yüklendiği belirlenir.
             </Label>
             <Label>Her Ürün İçin Tarih Giriniz.</Label>
-            <Label className={'text-muted-foreground text-xs m-0'}>Ürünler:</Label>
+            <Label className={'text-muted-foreground text-xs m-0'}>
+              Ürünler:
+            </Label>
 
-            {data && data?.Ürünler?.map((item, index) => <div key={item.id} className={'flex flex-col w-full border shadow-sm rounded p-2 gap-2'}>
-                <div className={'flex items-center gap-2 [&_span]:text-sm mx-auto w-fit'}>
-                    <span>{item.selectedCategoryValues}</span>
-                    -
-                    <span>{item.productName}</span>
-                    -
-                    <span>{item.productPrice} </span>
-                </div>
+            {data &&
+              data?.Orders?.map((item, index) => (
+                <div
+                  key={item.id}
+                  className={
+                    'flex flex-col w-full border shadow-sm rounded p-2 gap-2'
+                  }
+                >
+                  <div
+                    className={
+                      'flex items-center gap-2 [&_span]:text-sm mx-auto w-fit'
+                    }
+                  >
+                    <span>{data?.Ürünler[index]?.selectedCategoryValues}</span>-
+                    <span>{data?.Ürünler[index]?.productName}</span>-
+                    <span>{data?.Ürünler[index]?.productPrice} </span>
+                  </div>
 
-                <Popover>
+                  <Popover>
                     <PopoverTrigger asChild>
-                        <Button
-                            variant={"outline"}
-                            className={cn(
-                                "w-full justify-start text-left font-normal",
-                                !date[index] && "text-muted-foreground"
-                            )}
-                        >
-                            <CalendarIcon className="mr-2 h-4 w-4" />
-                            {date[index] ? format(date[index], "PPP") : <span>Pick a date</span>}
-                        </Button>
+                      <Button
+                        variant={'outline'}
+                        className={cn(
+                          'w-full justify-start text-left font-normal',
+                          !date[index] && 'text-muted-foreground'
+                        )}
+                      >
+                        <CalendarIcon className='mr-2 h-4 w-4' />
+                        {date[index] ? (
+                          format(date[index], 'PPP')
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                      </Button>
                     </PopoverTrigger>
-                    <PopoverContent className="w-full p-0">
-                        <Calendar
-                            mode="single"
-                            selected={date[index]}
-                            onSelect={(newDate) => setDate((prev) => ({...prev, [index]: newDate}))}
-                            initialFocus
-                        />
+                    <PopoverContent className='w-full p-0'>
+                      <Calendar
+                        name={`dates.${item.id}`}
+                        mode='single'
+                        selected={date[index]}
+                        onSelect={(newDate) => {
+                          setDate((prev) => ({ ...prev, [index]: newDate }));
+                          const dateStr = newDate ? newDate.toISOString() : '';
+                          props.setFieldValue(`dates.${index}`, {
+                            selectedDate: dateStr,
+                            selectedOrdersId: item.id,
+                          });
+                        }}
+                        initialFocus
+                      />
                     </PopoverContent>
-                </Popover>
-            </div>)}
+                  </Popover>
+                </div>
+              ))}
 
-            <Button className={"w-full"}>Tarihleri Guncelle</Button>
-        </div>
+            <Button className={'w-full'} type='submit'>
+              Tarihleri Guncelle
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </div>
-  )
-}
+  );
+};
 
-export default StepPage
+export default StepPage;
