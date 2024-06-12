08471a729eef9a59a77a5b4fecbf2d4e5d0fdaec
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-06-12 19:08:41 +0300

    api tamamen güncellendi

--- pages/api/stepByStep/urunMaliyetiVeCikisTarihi/index.js
+++ pages/api/stepByStep/urunMaliyetiVeCikisTarihi/index.js
@@ -1,36 +1,84 @@
-import { updateDataByAny } from '@/services/serviceOperations';
+import { updateDataByAny, getDataByMany } from '@/services/serviceOperations';
 
 const handler = async (req, res) => {
   try {
     if (req.method === 'POST') {
-      const { dates, tedarikciMaliyet, step, stepName } = req.body;
+      const { dates, step, stepName, tedarikciMaliyet, orderCode } = req.body;
+      const allStepBySteps = await getDataByMany('StepByStep', {
+        orderCode: orderCode,
+      });
+
+      dates.map((item) => {
+        const isoDate = item.selectedDate
+          ? new Date(item.selectedDate).toISOString()
+          : undefined;
+        return {
+          selectedDate: isoDate,
+          selectedOrdersId: item.selectedOrdersId,
+        };
+      });
 
       // Tüm güncelleme işlemlerini bir dizi içinde topla
-      const updatePromises = dates.map((item) => {
-        const { selectedDate, selectedOrdersId } = item;
-        const filtered = tedarikciMaliyet.filter(
-          (data) => data.selectedOrdersId === selectedOrdersId
+      const updatePromises = allStepBySteps.map((item, index) => {
+        const matchedDate = dates.find(
+          (dateItem) => dateItem?.selectedOrdersId === item.orderId
         );
 
-        return updateDataByAny(
-          'StepByStep',
-          {
-            orderId: selectedOrdersId,
-          },
-          {
-            urunCikisTarihi: selectedDate,
-            tedarikciMaliyeti: parseFloat(filtered[0].tedarikciMaliyeti),
-            step: step,
-            stepName: stepName,
+        if (matchedDate) {
+          // Eğer `dates` array'inde tarih varsa, bu değerle güncelle
+          const { selectedDate } = matchedDate;
+          if (selectedDate != undefined) {
+            return updateDataByAny(
+              'StepByStep',
+              { orderId: item.orderId },
+              {
+                urunCikisTarihi: new Date(selectedDate),
+                step: item.step > step ? item.step : step,
+                stepName: item.step > 5 ? item.stepName : 'Gümrük',
+                tedarikciMaliyeti: parseFloat(
+                  tedarikciMaliyet[index].tedarikciMaliyeti
+                ),
+              }
+            );
+          } else {
+            // Eğer `dates` array'inde tarih yoksa, tarih alanını sıfırla
+            let stepName = '';
+            if (item.step == 5) stepName = 'Ürün Maliyeti ve Çıkış Tarihi';
+            if (item.step < 5) stepName = item.stepName;
+            if (item.step > 5) stepName = 'Ürün Maliyeti ve Çıkış Tarihi';
+            return updateDataByAny(
+              'StepByStep',
+              { orderId: item.orderId },
+              {
+                urunCikisTarihi: null,
+                step: item.step >= 5 ? 5 : item.step,
+                stepName: stepName,
+              }
+            );
           }
-        );
+        } else {
+          // Eğer `dates` array'inde tarih yoksa, tarih alanını sıfırla
+          let stepName = '';
+          if (item.step == 5) stepName = 'Ürün Maliyeti ve Çıkış Tarihi';
+          if (item.step < 5) stepName = item.stepName;
+          if (item.step > 5) stepName = 'Ürün Maliyeti ve Çıkış Tarihi';
+          return updateDataByAny(
+            'StepByStep',
+            { orderId: item.orderId },
+            {
+              urunCikisTarihi: null,
+              step: item.step >= 5 ? 5 : item.step,
+              stepName: stepName,
+              tedarikciMaliyeti: parseFloat(1),
+            }
+          );
+        }
       });
 
       // Tüm güncellemeleri paralel olarak gerçekleştirmek için Promise.all kullan
       await Promise.all(updatePromises);
-
       return res.status(200).json({
-        message: 'Ürün maliyeti ve çıkış tarihi başarıyla kaydedildi!',
+        message: 'İşlem başarıyla gerçekleştirildi!',
         status: 'success',
       });
     } else {
