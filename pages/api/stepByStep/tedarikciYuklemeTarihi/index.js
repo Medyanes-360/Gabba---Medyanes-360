006a28979150c428613ed5283cac17d3da258da0
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-06-12 19:08:27 +0300

    api tamamen güncellendi.

--- pages/api/stepByStep/tedarikciYuklemeTarihi/index.js
+++ pages/api/stepByStep/tedarikciYuklemeTarihi/index.js
@@ -1,36 +1,85 @@
-import {
-  createNewDataMany,
-  getDataByUnique,
-  updateDataByAny,
-  updateDataByMany,
-} from '@/services/serviceOperations';
+import { getDataByMany, updateDataByAny } from '@/services/serviceOperations';
 
 const handler = async (req, res) => {
   try {
     if (req.method === 'POST') {
-      const { dates, step, stepName } = req.body;
-      // Tüm güncelleme işlemlerini bir dizi içinde topla
-      const updatePromises = dates.map((item) => {
-        const { selectedDate, selectedOrdersId } = item;
+      const { dates, step, stepName, orderCode } = req.body;
+      const allStepBySteps = await getDataByMany('StepByStep', {
+        orderCode: orderCode,
+      });
 
-        return updateDataByAny(
-          'StepByStep',
-          {
-            orderId: selectedOrdersId,
-          },
-          {
-            tedarikciYuklemeTarihi: new Date(selectedDate),
-            step: step,
-            stepName: stepName,
-          }
+      dates.map((item) => {
+        const isoDate = item.selectedDate
+          ? new Date(item.selectedDate).toISOString()
+          : undefined;
+        return {
+          selectedDate: isoDate,
+          selectedOrdersId: item.selectedOrdersId,
+        };
+      });
+
+      // Tüm güncelleme işlemlerini bir dizi içinde topla
+      const updatePromises = allStepBySteps.map((item) => {
+        const matchedDate = dates.find(
+          (dateItem) => dateItem?.selectedOrdersId === item.orderId
         );
+
+        if (matchedDate) {
+          // Eğer `dates` array'inde tarih varsa, bu değerle güncelle
+          const { selectedDate } = matchedDate;
+          if (selectedDate != undefined) {
+            return updateDataByAny(
+              'StepByStep',
+              { orderId: item.orderId },
+              {
+                tedarikciYuklemeTarihi: new Date(selectedDate),
+                step: item.step > step ? item.step : step,
+                stepName:
+                  item.step > 3
+                    ? item.stepName
+                    : 'Ürün Maliyeti ve Çıkış Tarihi',
+              }
+            );
+          } else {
+            // Eğer `dates` array'inde tarih yoksa, tarih alanını sıfırla
+            let stepName = '';
+            if (item.step == 3) stepName = 'Tedarikçi Yükleme Tarihi';
+            if (item.step < 3) stepName = item.stepName;
+            if (item.step > 3) stepName = 'Tedarikçi Yükleme Tarihi';
+            return updateDataByAny(
+              'StepByStep',
+              { orderId: item.orderId },
+              {
+                tedarikciYuklemeTarihi: null,
+                urunCikisTarihi: null,
+                step: item.step >= 3 ? 3 : item.step,
+                stepName: stepName,
+              }
+            );
+          }
+        } else {
+          // Eğer `dates` array'inde tarih yoksa, tarih alanını sıfırla
+          let stepName = '';
+          if (item.step == 3) stepName = 'Tedarikçi Yükleme Tarihi';
+          if (item.step < 3) stepName = item.stepName;
+          if (item.step > 3) stepName = 'Tedarikçi Yükleme Tarihi';
+          return updateDataByAny(
+            'StepByStep',
+            { orderId: item.orderId },
+            {
+              tedarikciYuklemeTarihi: null,
+              urunCikisTarihi: null,
+              step: item.step >= 3 ? 3 : item.step,
+              stepName: stepName,
+            }
+          );
+        }
       });
 
       // Tüm güncellemeleri paralel olarak gerçekleştirmek için Promise.all kullan
       await Promise.all(updatePromises);
-
       return res.status(200).json({
-        message: 'Tedarikçi yükleme tarihi başarıyla kaydedildi!',
+        message: 'İşlem başarıyla gerçekleştirildi!',
         status: 'success',
       });
     } else {
