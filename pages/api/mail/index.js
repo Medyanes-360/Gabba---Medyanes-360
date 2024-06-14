a0e1dcdb67d40ce097882fa5ee123fd2a1511d74
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-06-14 18:49:24 +0300

    logs added.

--- pages/api/mail/index.js
+++ pages/api/mail/index.js
@@ -1,7 +1,9 @@
-import nodemailer from "nodemailer";
+import { createNewData } from '@/services/serviceOperations';
+import nodemailer from 'nodemailer';
+import { getToken } from 'next-auth/jwt';
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
@@ -14,47 +16,54 @@ export const mailOptions = {
 
 const handler = async (req, res) => {
   //getServerSession:  Kullanıcının oturum açıp açmadığını kontrol eder. Eğer açılmışsa session değişkenine atar.
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     try {
-      const { email, url } = req.body;
+      const { email, url, orderCode } = req.body;
+      const userRole = token && token?.user?.role;
+      const userId = token && token?.user?.id;
 
-      //mail gönderme işlemi
+      // mail gönderme işlemi
       transporter.sendMail({
         ...mailOptions,
         subject: `TEST`,
         html: `
-                <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gabba - Sipariş fişiniz</title>
-</head>
-<body>
-<p>${url}</p>
-</body>
-</html>
-                `,
+                      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Gabba - Sipariş fişiniz</title>
+      </head>
+      <body>
+      <p>${url}</p>
+      </body>
+      </html>
+                      `,
         to: email,
       });
 
-      return res
-        .status(200)
-        .json({
-          status: "success",
-          message: "Şifre sıfırlama bağlantısı mail adresinize gönderildi.",
-        });
+      const response = await createNewData('Logs', {
+        role: userRole,
+        userId: userId,
+        step: 1.1,
+        stepName: 'Fişi Gör veya Gönder',
+        orderCode: orderCode,
+      });
+
+      return res.status(200).json({
+        status: 'success',
+        message: 'Şifre sıfırlama bağlantısı mail adresinize gönderildi.',
+      });
     } catch (error) {
-      return res.status(500).json({ status: "error", message: error.message });
+      return res.status(500).json({ status: 'error', message: error.message });
     }
   } else {
-    return res
-      .status(405)
-      .json({
-        status: "error",
-        message: "hatalı bir istek gerçekleştirdiniz.",
-      });
+    return res.status(405).json({
+      status: 'error',
+      message: 'hatalı bir istek gerçekleştirdiniz.',
+    });
   }
 };
 
