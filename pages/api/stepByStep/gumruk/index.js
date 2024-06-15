cfc279fee7c5d4d778198c344c7b8cad07297a14
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-06-15 17:55:01 +0300

    gümrük api created

--- pages/api/stepByStep/gumruk/index.js
+++ pages/api/stepByStep/gumruk/index.js
@@ -1,18 +1,41 @@
-import { updateDataByAny } from '@/services/serviceOperations';
+import {
+  updateDataByMany,
+  updateDataByAny,
+} from '@/services/serviceOperations';
 
 const handler = async (req, res) => {
   try {
     if (req.method === 'POST') {
-      const { step, stepName, orderCode } = req.body;
+      const { step, stepName, orderCode, checked } = req.body;
 
-      const response = await updateDataByAny(
+      const selectedOrderIds = Object.keys(checked).filter(
+        (orderId) => checked[orderId]
+      );
+
+      // Seçilen ürünlerin `gumruk` alanını true yap
+      if (selectedOrderIds?.length > 0) {
+        await updateDataByMany(
+          'StepByStep',
+          {
+            orderId: { in: selectedOrderIds },
+          },
+          {
+            step: step,
+            stepName: stepName,
+            gumruk: true,
+          }
+        );
+      }
+
+      // Seçilmeyen ürünlerin `gumruk` alanını false yap
+      await updateDataByMany(
         'StepByStep',
         {
           orderCode: orderCode,
+          orderId: { notIn: selectedOrderIds },
         },
         {
-          step: step,
-          stepName: stepName,
+          gumruk: false, // Seçilmeyenler için gumruk false
         }
       );
 
@@ -20,6 +43,21 @@ const handler = async (req, res) => {
         message: 'Ürün/Ürünler gümrüğe başarıyla kaydedildi!',
         status: 'success',
       });
+    } else if (req.method === 'PUT') {
+      const { id } = req.body;
+      const updated = await updateDataByAny(
+        'StepByStep',
+        {
+          orderId: id,
+        },
+        {
+          gumruk: false,
+        }
+      );
+      console.log(updated);
+      return res
+        .status(200)
+        .json({ status: 'success', message: 'Gümrük veriniz silindi!' });
     } else {
       throw new Error('Invalid Method!');
     }
