7896aca018cc81884ba50ac8210c568ab4663ce1
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-06-16 04:35:56 +0300

    Ürün takibi eklendi

--- app/(StepsLayout)/layout.jsx
+++ app/(StepsLayout)/layout.jsx
@@ -4,6 +4,19 @@ import { useParams } from 'next/navigation';
 import { getAPI } from '@/services/fetchAPI';
 import { useState, useEffect, createContext, useContext } from 'react';
 import LoadingScreen from '@/components/other/dashboardLoading';
+import { Button } from '@/components/ui/button';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { FaInfoCircle } from 'react-icons/fa';
 
 const LoadingContext = createContext();
 const StepByStepDataContext = createContext();
@@ -272,6 +285,136 @@ const StepsLayout = ({ children }) => {
                   </li>
                   <li>Teslim Edilen Ürün Sayısı: 0</li>
                 </ul>
+              </div>
+              {/* Ürün Bilgilerinin Takibi */}
+              <div className='w-full flex p-2 mt-2'>
+                <div className='flex items-center justify-end w-full text-gray-600 py-2'>
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button
+                        variant='outline'
+                        className='bg-slate-800 text-white hover:bg-slate-800/90 hover:text-white transition-all duration-200 ease-in-out'
+                      >
+                        Ürünler Hangi Adımda?
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle className='text-center'>
+                          Aşağıda ürünlerin hangi adımda kaldığını
+                          görebilirsiniz!
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          {orderData &&
+                            orderData?.Orders?.map((item, index) => {
+                              // stepByStepData'daki orderId ve step değerlerini kontrol et
+                              const matchingStepData = stepByStepData.find(
+                                (stepItem) =>
+                                  stepItem.orderId === item.id &&
+                                  stepItem.step >= 6
+                              );
+                              if (matchingStepData) {
+                                return (
+                                  <div
+                                    key={item.id}
+                                    className='flex flex-col w-full  
+                                     transition-all duration-200 ease-in-out border shadow-sm rounded p-2 gap-2 cursor-not-allowed bg-gray-800 text-white'
+                                  >
+                                    <div
+                                      className={
+                                        'flex items-center gap-2 [&_span]:text-sm w-full'
+                                      }
+                                    >
+                                      <FaInfoCircle
+                                        className='text-blue-600 ml-2'
+                                        size='20'
+                                      />
+                                      <span>
+                                        {
+                                          orderData?.Ürünler[index]
+                                            ?.selectedCategoryValues
+                                        }
+                                      </span>
+                                      -
+                                      <span>
+                                        {orderData?.Ürünler[index]?.productName}
+                                      </span>
+                                      -
+                                      <span>
+                                        {(item.productPrice +
+                                          item.productFeaturePrice) *
+                                          item.stock}
+                                      </span>
+                                    </div>
+                                    <p className='text-sm'>
+                                      Bu ürün {''}
+                                      <span className='p-1 rounded bg-gray-500'>
+                                        {matchingStepData.stepName}
+                                      </span>{' '}
+                                      kısmında kalmıştır.
+                                    </p>
+                                  </div>
+                                );
+                              }
+
+                              const doesntMatchData = stepByStepData.find(
+                                (stepItem) =>
+                                  stepItem.orderId === item.id &&
+                                  stepItem.step < 6
+                              );
+
+                              if (doesntMatchData) {
+                                return (
+                                  <div
+                                    key={item.id}
+                                    className='flex flex-col w-full  
+                                     transition-all duration-200 ease-in-out border shadow-sm rounded p-2 gap-2 cursor-not-allowed bg-gray-800 text-white'
+                                  >
+                                    <div
+                                      className={
+                                        'flex items-center gap-2 [&_span]:text-sm w-full'
+                                      }
+                                    >
+                                      <FaInfoCircle
+                                        className='text-blue-600 ml-2'
+                                        size='20'
+                                      />
+                                      <span>
+                                        {
+                                          orderData?.Ürünler[index]
+                                            ?.selectedCategoryValues
+                                        }
+                                      </span>
+                                      -
+                                      <span>
+                                        {orderData?.Ürünler[index]?.productName}
+                                      </span>
+                                      -
+                                      <span>
+                                        {(item.productPrice +
+                                          item.productFeaturePrice) *
+                                          item.stock}
+                                      </span>
+                                    </div>
+                                    <p className='text-sm'>
+                                      Bu ürün {''}
+                                      <span className='p-1 rounded bg-gray-500'>
+                                        {doesntMatchData.stepName}
+                                      </span>{' '}
+                                      kısmında kalmıştır.
+                                    </p>
+                                  </div>
+                                );
+                              }
+                            })}
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Kapat</AlertDialogCancel>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </div>
               </div>
               <div className='py-6 h-full w-full pl-6 md:pl-0'>{children}</div>
             </div>
