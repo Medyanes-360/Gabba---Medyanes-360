b960a4d5d10fb6beccdad82acc57c536f8192007
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-06-16 04:56:04 +0300

    Teklif eklendiğindeki hata düzeltildi. Sepet kısmında otomatik bir sonraki adıma geçiyordu.

--- components/createOffer/basketOffer/listBasket/index.js
+++ components/createOffer/basketOffer/listBasket/index.js
@@ -1,13 +1,11 @@
-"use client";
-import React, { useState } from "react";
-import { postAPI } from "@/services/fetchAPI";
-// import Customer from './Customer';
-import Customer from "@/components/customer";
-import CustomerRegistration from "@/components/customer/CustomerRegistration";
-import { IoChevronForwardOutline } from "react-icons/io5";
-import { Formik, Form, ErrorMessage } from "formik";
-import BasketCard from "./Card";
-import { useSession } from "next-auth/react";
+'use client';
+import React, { useState } from 'react';
+import { postAPI } from '@/services/fetchAPI';
+import Customer from '@/components/customer';
+import { IoChevronForwardOutline } from 'react-icons/io5';
+import { Formik, Form, ErrorMessage } from 'formik';
+import BasketCard from './Card';
+import { useSession } from 'next-auth/react';
 
 const ListBasket = ({
   toast,
@@ -24,7 +22,7 @@ const ListBasket = ({
   setHiddenBasketBar,
   setSelectedBasketFeatures,
   customers,
-  getData
+  getData,
 }) => {
   const { data } = useSession();
 
@@ -33,27 +31,28 @@ const ListBasket = ({
       <Formik
         //validationSchema={FinancialManagementValidationSchema}
         initialValues={{
-          orderNote: "",
-          ordersStatus: "Onay Bekliyor",
-          productOrderStatus: "Onay Bekliyor",
+          orderNote: '',
+          ordersStatus: 'Onay Bekliyor',
+          productOrderStatus: 'Onay Bekliyor',
           personelId: data?.user?.id,
-          customerId: "",
-          customerName: "",
+          customerId: '',
+          customerName: '',
         }}
         onSubmit={async (values, { resetForm }) => {
           setIsloading(true);
-          const response = await postAPI("/createOrder/order", {
+          const response = await postAPI('/createOrder/order', {
             basketData,
             values: {
               ...values,
             },
           });
 
-          if (response.status !== "success" || response.status == "error") {
+          if (response.status !== 'success' || response.status == 'error') {
             setIsloading(false);
             toast.error(response.message);
           } else {
             setIsloading(false);
+            setIsCustomerAndPersonel(false);
             toast.success(response.message);
             resetForm();
             setBasketData([]);
@@ -63,55 +62,62 @@ const ListBasket = ({
         }}
       >
         {(FormProps) => (
-          <Form onSubmit={FormProps.handleSubmit} id="createOfferForm">
-            <div className="flex flex-col justify-center items-center">
+          <Form onSubmit={FormProps.handleSubmit} id='createOfferForm'>
+            <div className='flex flex-col justify-center items-center'>
               {isCustomerAndPersonel ? (
-                <Customer toast={toast} getData={getData} FormProps={FormProps} customers={customers} />
+                <Customer
+                  toast={toast}
+                  getData={getData}
+                  FormProps={FormProps}
+                  customers={customers}
+                />
               ) : (
                 <>
                   {basketData.length === 0 ? (
-                    <div className="grid grid-cols-1 w-full">
-                      <div className="border p-3 mx-4 rounded shadow order-2 md:order-1">
-                        <p className="text-2xl font-bold text-center my-4 text-red-500">
+                    <div className='grid grid-cols-1 w-full'>
+                      <div className='border p-3 mx-4 rounded shadow order-2 md:order-1'>
+                        <p className='text-2xl font-bold text-center my-4 text-red-500'>
                           Sepetinizde ürün bulunmamakta!
                         </p>
                       </div>
                     </div>
                   ) : (
                     <>
-                      <h1 className="text-2xl font-bold text-center my-4 uppercase">
+                      <h1 className='text-2xl font-bold text-center my-4 uppercase'>
                         Sepet
                       </h1>
-                      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-4 p-4">
-                        {basketData && basketData.length > 0 && basketData?.map((item) => (
-                          <BasketCard
-                            key={item.id}
-                            item={item}
-                            productFeatures={productFeatures}
-                            setBasketData={setBasketData}
-                            setIsloading={setIsloading}
-                            toast={toast}
-                            basketData={basketData}
-                            setSelectedBasketData={setSelectedBasketData}
-                            setIsSelectedBasket={setIsSelectedBasket}
-                            setHiddenBasketBar={setHiddenBasketBar}
-                            setSelectedBasketFeatures={
-                              setSelectedBasketFeatures
-                            }
-                          />
-                        ))}
+                      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-4 p-4'>
+                        {basketData &&
+                          basketData.length > 0 &&
+                          basketData?.map((item) => (
+                            <BasketCard
+                              key={item.id}
+                              item={item}
+                              productFeatures={productFeatures}
+                              setBasketData={setBasketData}
+                              setIsloading={setIsloading}
+                              toast={toast}
+                              basketData={basketData}
+                              setSelectedBasketData={setSelectedBasketData}
+                              setIsSelectedBasket={setIsSelectedBasket}
+                              setHiddenBasketBar={setHiddenBasketBar}
+                              setSelectedBasketFeatures={
+                                setSelectedBasketFeatures
+                              }
+                            />
+                          ))}
                       </div>
-                      <div className="flex justify-center mb-3 items-center w-full">
+                      <div className='flex justify-center mb-3 items-center w-full'>
                         <button
                           onClick={() => setIsCustomerAndPersonel(true)}
-                          type="button"
-                          className="hover:scale-105 transition-all flex justify-center items-center p-2 text-white font-semibold bg-gray-800 rounded group
-                  "
+                          type='button'
+                          className='hover:scale-105 transition-all flex justify-center items-center p-2 text-white font-semibold bg-gray-800 rounded group
+                  '
                         >
                           İleri
                           <IoChevronForwardOutline
                             size={22}
-                            className="text-white transform translate-x-0 group-hover:translate-x-2 transition-transform"
+                            className='text-white transform translate-x-0 group-hover:translate-x-2 transition-transform'
                           />
                         </button>
                       </div>
