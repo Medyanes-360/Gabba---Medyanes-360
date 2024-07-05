097e7694312c6ecc2482329f5eede8bdb8876fe6
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-07-05 20:40:26 +0300

    Baba adı eklendi

--- components/customer/index.jsx
+++ components/customer/index.jsx
@@ -1,7 +1,7 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import Pagination from "./Pagination";
-import FilterInput from "./FilterInput";
+'use client';
+import React, { useState, useEffect } from 'react';
+import Pagination from './Pagination';
+import FilterInput from './FilterInput';
 import {
   Table,
   TableBody,
@@ -9,11 +9,11 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { Label } from "../ui/label";
+} from '@/components/ui/table';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+import { Label } from '../ui/label';
 import {
   Dialog,
   DialogClose,
@@ -23,12 +23,12 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Form, Formik } from "formik";
-import { postAPI } from "@/services/fetchAPI";
-import { BiPlus } from "react-icons/bi";
-import { useLoadingContext } from "@/app/(HomeLayout)/layout";
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Form, Formik } from 'formik';
+import { postAPI } from '@/services/fetchAPI';
+import { BiPlus } from 'react-icons/bi';
+import { useLoadingContext } from '@/app/(HomeLayout)/layout';
 
 const MusteriTablosu = ({
   currentPage,
@@ -40,8 +40,8 @@ const MusteriTablosu = ({
 }) => {
   const handleMusteriSecimi = (customer, FormProps) => {
     // Formik formundan gelen props değerlerini kullanarak müşteri bilgilerini güncelleyin
-    FormProps.setFieldValue("customerId", customer.id);
-    FormProps.setFieldValue("customerName", customer.name);
+    FormProps.setFieldValue('customerId', customer.id);
+    FormProps.setFieldValue('customerName', customer.name);
 
     setSelectedMusteri(customer);
   };
@@ -53,38 +53,41 @@ const MusteriTablosu = ({
   );
 
   return (
-    <div className="overflow-x-auto w-[600px] lg:w-full rounded border border-border mt-8">
-      <Table className="border-collapse w-full">
+    <div className='overflow-x-auto w-[600px] lg:w-full rounded border border-border mt-8'>
+      <Table className='border-collapse w-full'>
         <TableHeader>
-          <TableHead className="border border-border">Seçim</TableHead>
-          <TableHead className="border border-border">Firma İsmi</TableHead>
-          <TableHead className="border border-border">Müşterinin Adı</TableHead>
-          <TableHead className="border border-border">
+          <TableHead className='border border-border'>Seçim</TableHead>
+          <TableHead className='border border-border'>Firma İsmi</TableHead>
+          <TableHead className='border border-border'>Müşterinin Adı</TableHead>
+          <TableHead className='border border-border'>
             Müşterinin Soyadı
           </TableHead>
-          <TableHead className="border border-border">
+          <TableHead className='border border-border'>
             Müşterinin Adresi
           </TableHead>
-          <TableHead className="border border-border">
+          <TableHead className='border border-border'>
             Müşterinin Mail Adresi
           </TableHead>
-          <TableHead className="border border-border">
+          <TableHead className='border border-border'>
             Müşterinin Telefon Numarası
           </TableHead>
+          <TableHead className='border border-border'>
+            Müşterinin Baba Adı
+          </TableHead>
         </TableHeader>
-        <TableBody className="text-center">
+        <TableBody className='text-center'>
           {currentMusteriler.map((musteri, index) => (
             <TableRow
               key={index}
               className={`${
                 musteri.name + musteri.phoneNumber ===
                   selectedMusteri?.name + selectedMusteri?.phoneNumber &&
-                "bg-green-200 hover:bg-green-300 transition-all duration-200 ease-in-out"
+                'bg-green-200 hover:bg-green-300 transition-all duration-200 ease-in-out'
               }`}
             >
-              <TableCell className="border border-border">
+              <TableCell className='border border-border'>
                 <Checkbox
-                  className="w-5 h-5"
+                  className='w-5 h-5'
                   checked={
                     selectedMusteri ? selectedMusteri.id === musteri.id : false
                   }
@@ -93,23 +96,26 @@ const MusteriTablosu = ({
                   }
                 />
               </TableCell>
-              <TableCell className="border border-border">
+              <TableCell className='border border-border'>
                 {musteri.company_name}
               </TableCell>
-              <TableCell className="border border-border">
+              <TableCell className='border border-border'>
                 {musteri.name}
               </TableCell>
-              <TableCell className="border border-border">
+              <TableCell className='border border-border'>
                 {musteri.surname}
               </TableCell>
-              <TableCell className="border border-border">
+              <TableCell className='border border-border'>
                 {musteri.address}
               </TableCell>
-              <TableCell className="border border-border">
+              <TableCell className='border border-border'>
                 {musteri.mailAddress}
               </TableCell>
-              <TableCell className="border border-border">
+              <TableCell className='border border-border'>
                 {musteri.phoneNumber}
+              </TableCell>
+              <TableCell className='border border-border'>
+                {musteri.father_name}
               </TableCell>
             </TableRow>
           ))}
@@ -119,7 +125,13 @@ const MusteriTablosu = ({
   );
 };
 
-const Customer = ({ setAddCustomerPopup, customers, FormProps, getData, toast }) => {
+const Customer = ({
+  setAddCustomerPopup,
+  customers,
+  FormProps,
+  getData,
+  toast,
+}) => {
   const [currentPage, setCurrentPage] = useState(1);
   const { isLoading, setIsLoading } = useLoadingContext();
   const [selectedMusteri, setSelectedMusteri] = useState(null);
@@ -132,16 +144,16 @@ const Customer = ({ setAddCustomerPopup, customers, FormProps, getData, toast })
   }, [customers]);
 
   return (
-    <div className="m-5">
-      <div className="flex flex-col gap-2 mb-12">
-        <Label className="text-xl font-semibold">
+    <div className='m-5'>
+      <div className='flex flex-col gap-2 mb-12'>
+        <Label className='text-xl font-semibold'>
           Müşteri Tanımlama Paneli
         </Label>
-        <Label className="text-gray-700 font-medium">
+        <Label className='text-gray-700 font-medium'>
           Bu sayfada siparişi hangi müşteriniz için oluşturduğunuzu belirleyiniz
         </Label>
       </div>
-      <div className="flex flex-col gap-2 md:flex-row md:gap-0 md:justify-between items-center">
+      <div className='flex flex-col gap-2 md:flex-row md:gap-0 md:justify-between items-center'>
         <FilterInput
           customers={customers}
           setFilteredMusteriler={setFilteredMusteriler}
@@ -159,14 +171,14 @@ const Customer = ({ setAddCustomerPopup, customers, FormProps, getData, toast })
               */}
 
             <Button
-              title="Yeni müşteri kayıt et."
-              type="button"
-              className="text-white w-10 h-10 flex items-center justify-center p-0 rounded-full font-semibold transition-all duration-300"
+              title='Yeni müşteri kayıt et.'
+              type='button'
+              className='text-white w-10 h-10 flex items-center justify-center p-0 rounded-full font-semibold transition-all duration-300'
             >
               <BiPlus size={24} />
             </Button>
           </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
+          <DialogContent className='sm:max-w-[425px]'>
             <DialogHeader>
               <DialogTitle>Müşteri Kaydı Ekle</DialogTitle>
               <DialogDescription>
@@ -174,28 +186,29 @@ const Customer = ({ setAddCustomerPopup, customers, FormProps, getData, toast })
                 doldurunuz.
               </DialogDescription>
             </DialogHeader>
-            <div className="grid gap-4 py-4">
+            <div className='grid gap-4 py-4'>
               <Formik
                 initialValues={{
-                  name: "",
-                  surname: "",
-                  mailAddress: "",
-                  address: "",
-                  company_name: "",
-                  phoneNumber: "",
+                  name: '',
+                  surname: '',
+                  mailAddress: '',
+                  address: '',
+                  company_name: '',
+                  phoneNumber: '',
+                  father_name: '',
                 }}
                 onSubmit={async (values, { resetForm }) => {
-                  setOpenModal(false)
+                  setOpenModal(false);
                   setIsLoading(true);
-                  const response = await postAPI("/customer", values).then(
+                  const response = await postAPI('/customer', values).then(
                     (res) => {
-                      if (res.status == "error") {
+                      if (res.status == 'error') {
                         setIsLoading(false);
                         return toast.error(res.message);
                       }
-                      if (res.status == "success") {
+                      if (res.status == 'success') {
                         setIsLoading(false);
-                        getData("onlyCustomer");
+                        getData('onlyCustomer');
                         resetForm();
                         setAddCustomerPopup(false);
                         return toast.success(res.message);
@@ -207,44 +220,50 @@ const Customer = ({ setAddCustomerPopup, customers, FormProps, getData, toast })
               >
                 {(props) => (
                   <Form onSubmit={props.handleSubmit}>
-                    <div className="flex flex-col gap-2.5">
+                    <div className='flex flex-col gap-2.5'>
                       <Input
-                        type="text"
-                        name="company_name"
+                        type='text'
+                        name='company_name'
                         onChange={props.handleChange}
-                        placeholder="Firma İsmi"
+                        placeholder='Firma İsmi'
                       />
                       <Input
-                        type="text"
-                        name="name"
+                        type='text'
+                        name='name'
                         onChange={props.handleChange}
-                        placeholder="Ad"
+                        placeholder='Ad'
                       />
                       <Input
-                        type="text"
-                        name="surname"
+                        type='text'
+                        name='surname'
                         onChange={props.handleChange}
-                        placeholder="Soyad"
+                        placeholder='Soyad'
                       />
                       <Input
-                        type="text"
-                        name="address"
+                        type='text'
+                        name='address'
                         onChange={props.handleChange}
-                        placeholder="Adres"
+                        placeholder='Adres'
                       />
                       <Input
-                        type="text"
-                        name="mailAddress"
+                        type='text'
+                        name='mailAddress'
                         onChange={props.handleChange}
-                        placeholder="Mail Adresi"
+                        placeholder='Mail Adresi'
                       />
                       <Input
-                        type="text"
-                        name="phoneNumber"
+                        type='text'
+                        name='phoneNumber'
                         onChange={props.handleChange}
-                        placeholder="Telefon Numarası"
+                        placeholder='Telefon Numarası'
                       />
-                      <Button type="submit" className="mt-2">
+                      <Input
+                        type='text'
+                        name='father_name'
+                        onChange={props.handleChange}
+                        placeholder='Baba Adı'
+                      />
+                      <Button type='submit' className='mt-2'>
                         Yeni Müşteri Ekle
                       </Button>
                     </div>
@@ -273,18 +292,18 @@ const Customer = ({ setAddCustomerPopup, customers, FormProps, getData, toast })
         setCurrentPage={setCurrentPage}
         pageSize={pageSize}
       />
-      <div className="flex justify-between mt-4 gap-6">
+      <div className='flex justify-between mt-4 gap-6'>
         <Textarea
-          name="orderNote"
+          name='orderNote'
           onChange={FormProps.handleChange}
-          className="w-full h-[100px] resize-none border rounded"
-          placeholder="Ürün için genel açıklama notu ekleyin..."
+          className='w-full h-[100px] resize-none border rounded'
+          placeholder='Ürün için genel açıklama notu ekleyin...'
         />
         {selectedMusteri && (
           <button
             onSubmit={FormProps.handleSubmit}
-            type="submit"
-            className="w-1/4 bg-green-500 rounded text-white font-semibold p-3"
+            type='submit'
+            className='w-1/4 bg-green-500 rounded text-white font-semibold p-3'
           >
             Sipariş Oluştur
           </button>
